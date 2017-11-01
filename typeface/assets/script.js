b377bae7f873e3c3353e0b0282946b784b6516e4
mbuenafe <Mark@The-Markbook.local>
2017-10-31 23:56:54 -0700

    Typeface
    Typeface upload

---
+++ typeface/assets/script.js
@@ -0,0 +1,12 @@
+    $(document).ready(function () {
+    //     $('#edition2014').click(function () {
+    //         $('.menu_edition_2014').slideToggle("slow");
+    //         $('#menu').toggleClass('fade');
+    //     });
+    // });
+
+
+    // $('.A').on('click', function() {
+    //         $(".parallelogram2").toggleClass('parallelogram2.1');
+
+});
